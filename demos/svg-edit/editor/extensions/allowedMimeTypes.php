e5d01cf895c912b9d1ce64e8abcf61b33c2bfbe2
Brett Zamir <brettz9@yahoo.com>
2014-06-15 07:13:09 +0800

    hack for Notepad++ custom MD editor (and SVG update)

--- demos/svg-edit/editor/extensions/allowedMimeTypes.php
+++ demos/svg-edit/editor/extensions/allowedMimeTypes.php
@@ -1,11 +1,12 @@
 <?php
 
 $allowedMimeTypesBySuffix = array(
-    'svg' => 'image/svg+xml;charset=utf-8',
+    'svg' => 'image/svg+xml;charset=UTF-8',
     'png' => 'image/png',
     'jpeg' => 'image/jpeg',
     'bmp' => 'image/bmp',
-    'webp' => 'image/webp'
+    'webp' => 'image/webp',
+	'pdf' => 'application/pdf'
 );
 
 ?>
