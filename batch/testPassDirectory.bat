6c766ff92ba0a2f97a7291bf25395ccfb6a8d5f6
Brett Zamir <brettz9@yahoo.com>
2014-06-23 22:10:46 +0800

    Ensure opens in new tab instead of existing window

--- batch/testPassDirectory.bat
+++ batch/testPassDirectory.bat
@@ -1,3 +1,3 @@
 :: For details on where one may put this, http://superuser.com/a/722699/156958 and http://answers.microsoft.com/en-us/windows/forum/windows_vista-desktop/how-to-locate-the-sendto-folder-in-vista/78b16711-1135-4eb0-851a-8abae9bfe9ed
 :: e.g., the location, C:\Users\Brett\AppData\Roaming\Microsoft\Windows\SendTo
-"c:\Program Files (x86)\Mozilla Firefox\firefox.exe" -webappdir "%1" -webappsite "http://brett-zamir.me/webappfind/demos/dir.html?path=%path"
+"c:\Program Files (x86)\Mozilla Firefox\firefox.exe" -remote "openurl(about:newtab)" -webappdir "%1" -webappsite "http://brett-zamir.me/webappfind/demos/dir.html?path=%path"
