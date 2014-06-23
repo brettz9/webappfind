f40aaa52999a8ecaeb935d1691820b2318006280
Brett Zamir <brettz9@yahoo.com>
2014-06-23 21:54:33 +0800

    Add demo batch (e.g., for use with SendTo); again, only probably useful for AsYouWish at this point

---
+++ batch/testPassDirectory.bat
@@ -0,0 +1,2 @@
+:: See http://superuser.com/a/722699/156958 and http://answers.microsoft.com/en-us/windows/forum/windows_vista-desktop/how-to-locate-the-sendto-folder-in-vista/78b16711-1135-4eb0-851a-8abae9bfe9ed
+"c:\Program Files (x86)\Mozilla Firefox\firefox.exe" -webappdir "%1" -webappsite "http://brett-zamir.me/webappfind/demos/dir.html?path=%path"
