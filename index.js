0aca0f0e16f2aea447c34b2c824070d46ec4f4a5
Brett Zamir <brettz9@yahoo.com>
2016-10-12 23:50:37 -0700

    testing

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 /*
 Code todos
 
-1. Dynamically generate "path" in waf.json
+0. Dynamically generate "path" in waf.json
+
+1. Listen for arguments and pass on
 */
 
 
 console.log('"hello2"');
 console.log('["abc"]');
+console.log(
+    process.argv.slice(2)
+);
+console.log(
+    process.argv.slice(0, 2)
+);
