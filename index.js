05545b403109af98e6465850572bbc968cfc54cd
Brett Zamir <brettz9@yahoo.com>
2016-10-12 23:37:11 -0700

    Add some listening code boilerplate within add-on

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+console.log('started webappfind');
 
-console.log('hello2');
+/**
+On startup, connect to the "webappfind" app.
+*/
+var port = browser.runtime.connectNative("webappfind");
+
+/**
+Listen for messages from the webappfind app (which listens for command line invocations, ala "Open"/"Open with...").
+*/
+port.onMessage.addListener((response) => {
+    console.log("Received: " + response);
+});
+
+
+// Post back from page under correct conditions (for file writing, we can do in this file instead, however)
+// port.postMessage("ping");
