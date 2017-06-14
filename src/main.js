ed6b258750cbdba8e41cf4128d1c8d139d387f27
Brett Zamir <brettz9@yahoo.com>
2017-06-14 20:51:34 +0800

    - Linting: Add ESLint - Working registering/adding app manifest file

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ See:
 
 */
 
-const allUsers = false;
+const allUsers = !!process.argv[2];
 const path = require('path');
+const fs = require('fs');
 
 const extensionName = 'webappfind'; // Also used for JSON file name
 
 const isWin = /^win/.test(process.platform);
 const isMac = process.platform === 'darwin';
+const isLinux = process.platform === 'linux';
 if (isWin) {
     // Todo: UNTESTED!!!
     const regedit = require('regedit');
@@ -22,27 +24,37 @@ if (isWin) {
     regedit.putValue({
         [regKey]: {
             appManifestPath: { // Name (here `appManifestPath`) is unused for defaults
-                value: path.resolve(`./${extensionName}.json`), // e.g., C:\\webappfind.json
+                value:
+                    // e.g., C:\\webappfind.json
+                    path.join(__dirname, extensionName + '.json'),
                 type: 'REG_DEFAULT'
             }
         }
-    }, function (err) {
+    }, (err) => {
         if (err) {
-            console.log('Erroring saving to (Windows) registry.')
+            console.log('Erroring saving to (Windows) registry.');
             return;
         }
         console.log('Saved to Windows registry');
     });
-} else if (isMac) {
-    const manifestPath = (allUsers ? '' : '~') +
-        `/Library/Application Support/Mozilla/NativeMessagingHosts/${extensionName}.json`;
-    console.log('manifestPath', manifestPath);
-} else if (isLinux) {
-    // Todo: UNTESTED!!! (but should work)
-    const manifestPath = (allUsers
-        ? '/usr/lib/' // Could also be '/usr/lib64/'
-        : '~/.'
-    ) + `mozilla/native-messaging-hosts/${extensionName}.json`;
+} else if (isMac || isLinux) {
+    const manifestDirectory = isMac
+        ? (allUsers ? '' : '~') +
+            `/Library/Application Support/Mozilla/NativeMessagingHosts/`
+        // Todo: Untested on Linux (but should be ok)
+        : (allUsers
+            ? '/usr/lib/' // Could also be '/usr/lib64/'
+            : '~/.'
+        ) + `mozilla/native-messaging-hosts/`;
+
+    fs.mkdir(manifestDirectory, (err) => {
+        if (err && err.code !== 'EEXIST') {
+            console.log('Error saving manifest directory (' + (isMac ? 'Mac' : 'Linux') + ')', err);
+        }
+        fs.createReadStream(`${path.join(__dirname, extensionName)}.json`).pipe(
+            fs.createWriteStream(`${manifestDirectory + extensionName}.json`)
+        );
+    });
 } else {
     console.log('Unsupported OS!');
     process.exit();
