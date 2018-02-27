ae0630771f48108f30c919fefa67bafd1b54378c
Brett Zamir <brettz9@yahoo.com>
2018-02-27 20:03:37 +0800

    - Leverage executable file for passing messages to server (and later for building executables)

--- native-app.js
+++ native-app.js
@@ -1,6 +1,30 @@
 const nativeMessage = require('chrome-native-messaging');
 const WebSocket = require('ws');
 
+const argType = process.argv[2];
+const clientMessage = process.argv[3];
+
+(() => {
+switch (argType) { // We're reusing this executable to accept messages for setting up a client
+case 'webappfind':
+case 'webappfind-client': {
+    const ws = new WebSocket('ws://localhost:8080');
+
+    ws.on('open', () => {
+        ws.send(clientMessage);
+    });
+
+    ws.on('message', (data) => {
+        // console.log('msg recd by client: ' + data);
+        ws.close();
+    });
+    return;
+} case 'build-openwith-exec': {
+    // Todo: Allow calling this functionality from within the add-on
+    return;
+}
+}
+
 const input = new nativeMessage.Input();
 const transform = new nativeMessage.Transform(messageHandler);
 const output = new nativeMessage.Output();
@@ -29,3 +53,4 @@ wss.on('connection', (ws) => {
         ws.send('Native app server sending back: ' + msg);
     });
 });
+})();
