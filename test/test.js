07ad01186c11a6adc39de05389bd0742ceef473d
Brett Zamir <brettz9@yahoo.com>
2018-02-27 12:24:00 +0800

    - Testing: Add Mocha/Chai with test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,20 @@
-const WebSocket = require('ws');
-const ws = new WebSocket('ws://localhost:8080');
+/* eslint-env mocha */
+const {expect} = require('chai');
 
-ws.on('open', () => {
-    ws.send('msg from client');
-});
+describe('Native messaging communication', function () {
+    it('should send and receive messages', (done) => {
+        const WebSocket = require('ws');
+        const ws = new WebSocket('ws://localhost:8080');
 
-ws.on('message', (data) => {
-    console.log('msg recd by client: ' + data);
+        ws.on('open', () => {
+            ws.send('msg from client');
+        });
+
+        ws.on('message', (data) => {
+            console.log('msg recd by client: ' + data);
+            expect(data).to.equal('server sending something');
+            ws.close();
+            done();
+        });
+    });
 });
