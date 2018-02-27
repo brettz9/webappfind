403d6c19df9d4a7ee9782b32e78f8ccdbbcd7c05
Brett Zamir <brettz9@yahoo.com>
2018-02-27 11:32:07 +0800

    - Linting: ESLint ignore file (and avoid warning about ignore) - Testing: Have browser console show up automatically in webext script - Building: Add web-ext ignore file

---
+++ web-ext-config.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+'use strict';
+module.exports = {
+    verbose: true,
+    ignoreFiles: [
+        '.DS_Store',
+        '.eslintrc',
+        '.eslintignore',
+        '.remarkrc',
+        '*.zip',
+        '*.xpi',
+        'web-ext-artifacts/**'
+    ]
+};
