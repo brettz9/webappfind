80f57171bd6bf7eed8cd2279b7cca5bd723e4689
Brett Zamir <brettz9@yahoo.com>
2018-03-04 19:36:10 +0800

    - Fix polyfill copying

--- copy-polyfill.js
+++ copy-polyfill.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const {copyFile} = require('./polyfills/promise-wrappers');
-const polyfillFile = 'polyfills/browser-polyfill.js'; // could end in .min.js instead
+const polyfillFile = 'browser-polyfill.js'; // could end in .min.js instead
 copyFile(
     path.join(
         __dirname,
         'node_modules/webextension-polyfill/dist/',
         polyfillFile
     ),
-    path.join(__dirname, polyfillFile)
+    path.join(__dirname, 'polyfills', polyfillFile)
 ).then(() => {
     console.log('Copied polyfill file.');
 }).catch((err) => {
