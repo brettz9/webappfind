4331c0f61496c081410759a81cc71c87da83d4ec
Brett Zamir <brettz9@yahoo.com>
2018-09-05 10:23:21 +0800

    - Force options page to rebuild upon preference changes (as auto-approving will need to change prefs)

--- options/options.js
+++ options/options.js
@@ -1,5 +1,6 @@
 /* eslint-env browser, webextensions */
 import {jml, $} from './jml.js';
+import {DOM} from '../utils/templateUtils.js';
 
 const nbsp = '\u00a0';
 
@@ -7,109 +8,119 @@ function _ (...args) {
     return browser.i18n.getMessage(...args);
 }
 
-document.title = _('extensionName'); // If switch to tabs
-(async () => {
-jml('section', await Promise.all([
-    ['autoApproveWAFOpenedSites', 'checkbox', true],
-    ['allowedSites', 'addRemove', []]
-].map(async ([preferenceKey, type, defaultValue]) => {
-    let config = {};
-    try {
-        ({[preferenceKey]: config = {}} = await browser.storage.local.get(preferenceKey));
-    } catch (err) {}
+browser.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName !== 'local') {
+        return;
+    }
+    buildOptionsPage();
+});
 
-    switch (type) {
-    case 'addRemove': {
-        return ['div', {class: 'addRemove'}, [
-            _(preferenceKey + '_title'),
-            ['section', {class: 'addon-description'}, [
-                _(preferenceKey + '_description')
-            ]],
-            ['br'],
-            ['div', {
-                class: 'addRemoveInner'
-            }, [
-                ['input', {
-                    id: `addRemove_${preferenceKey}input`,
-                    type: 'url'
-                }],
-                ' ' + nbsp,
-                ['button', {
-                    $on: {
-                        click () {
-                            const input = $(`#addRemove_${preferenceKey}input`);
-                            const {origin, pathname, search} = new URL(input.value);
-                            // Exclude the hash (should we also exclude `search`?)
-                            const newVal = origin + pathname + search;
-                            const select = $(`#addRemove_${preferenceKey}select`);
-                            if (![...select.options].some(({value}) => {
-                                return value === newVal;
-                            })) {
-                                select.add(jml('option', [newVal]));
+document.title = _('extensionName'); // If switch to tabs
+async function buildOptionsPage () {
+    DOM.removeChildren('body');
+
+    jml('section', await Promise.all([
+        ['autoApproveWAFOpenedSites', 'checkbox', true],
+        ['allowedSites', 'addRemove', []]
+    ].map(async ([preferenceKey, type, defaultValue]) => {
+        let config = {};
+        try {
+            ({[preferenceKey]: config = {}} = await browser.storage.local.get(preferenceKey));
+        } catch (err) {}
+
+        switch (type) {
+        case 'addRemove': {
+            return ['div', {class: 'addRemove'}, [
+                _(preferenceKey + '_title'),
+                ['section', {class: 'addon-description'}, [
+                    _(preferenceKey + '_description')
+                ]],
+                ['br'],
+                ['div', {
+                    class: 'addRemoveInner'
+                }, [
+                    ['input', {
+                        id: `addRemove_${preferenceKey}input`,
+                        type: 'url'
+                    }],
+                    ' ' + nbsp,
+                    ['button', {
+                        $on: {
+                            click () {
+                                const input = $(`#addRemove_${preferenceKey}input`);
+                                const {origin, pathname, search} = new URL(input.value);
+                                // Exclude the hash (should we also exclude `search`?)
+                                const newVal = origin + pathname + search;
+                                const select = $(`#addRemove_${preferenceKey}select`);
+                                if (![...select.options].some(({value}) => {
+                                    return value === newVal;
+                                })) {
+                                    select.add(jml('option', [newVal]));
+                                    select.$savePref();
+                                }
+                            }
+                        }
+                    }, [_('options_addRemove_add')]],
+                    ' ',
+                    ['button', {
+                        $on: {
+                            click () {
+                                const select = $(`#addRemove_${preferenceKey}select`);
+                                select.remove(select.selectedIndex);
                                 select.$savePref();
                             }
                         }
-                    }
-                }, [_('options_addRemove_add')]],
-                ' ',
-                ['button', {
-                    $on: {
-                        click () {
-                            const select = $(`#addRemove_${preferenceKey}select`);
-                            select.remove(select.selectedIndex);
-                            select.$savePref();
+                    }, [_('options_addRemove_remove')]],
+                    ['br'],
+                    ['br'],
+                    ['select', {
+                        id: `addRemove_${preferenceKey}select`,
+                        class: 'addRemove',
+                        size: 20,
+                        readonly: true,
+                        $custom: {
+                            async $savePref () {
+                                const optionValues = [...this.options].map(({value}) => {
+                                    return value;
+                                });
+                                await browser.storage.local.set({
+                                    [preferenceKey]: {
+                                        optionValues
+                                    }
+                                });
+                            }
                         }
-                    }
-                }, [_('options_addRemove_remove')]],
-                ['br'],
-                ['br'],
-                ['select', {
-                    id: `addRemove_${preferenceKey}select`,
-                    class: 'addRemove',
-                    size: 20,
-                    readonly: true,
-                    $custom: {
-                        async $savePref () {
-                            const optionValues = [...this.options].map(({value}) => {
-                                return value;
-                            });
+                    }, (config.optionValues || defaultValue).map((value) => {
+                        return ['option', [value]];
+                    })]
+                ]]
+            ]];
+        }
+        case 'checkbox': {
+            const {enabled = defaultValue} = config;
+            return ['label', [
+                ['input', {
+                    type: 'checkbox',
+                    checked: enabled,
+                    $on: {
+                        async change ({target}) {
                             await browser.storage.local.set({
                                 [preferenceKey]: {
-                                    optionValues
+                                    enabled: target.checked
                                 }
                             });
                         }
                     }
-                }, (config.optionValues || defaultValue).map((value) => {
-                    return ['option', [value]];
-                })]
-            ]]
-        ]];
-    }
-    case 'checkbox': {
-        const {enabled = defaultValue} = config;
-        return ['label', [
-            ['input', {
-                type: 'checkbox',
-                checked: enabled,
-                $on: {
-                    async change ({target}) {
-                        await browser.storage.local.set({
-                            [preferenceKey]: {
-                                enabled: target.checked
-                            }
-                        });
-                    }
-                }
-            }],
-            ' ',
-            _(preferenceKey + '_title'),
-            ['section', {class: 'addon-description'}, [
-                _(preferenceKey + '_description')
-            ]],
-            ['br']
-        ]];
-    }
-    }
-})), document.body);
-})();
+                }],
+                ' ',
+                _(preferenceKey + '_title'),
+                ['section', {class: 'addon-description'}, [
+                    _(preferenceKey + '_description')
+                ]],
+                ['br']
+            ]];
+        }
+        }
+    })), document.body);
+};
+buildOptionsPage();
